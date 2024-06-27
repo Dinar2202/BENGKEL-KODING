1b08913f72809a61ef51583f3289d7d08d4a2ff2
Dinar2202 <102625453+Dinar2202@users.noreply.github.com>
2024-06-27 13:36:50 +0700

    create.php

--- create.php
+++ create.php
@@ -20,15 +20,14 @@
     //Cek apakah ada kiriman form dari method post
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-        $nama=input($_POST["nama"]);
-        $sekolah=input($_POST["sekolah"]);
-        $jurusan=input($_POST["jurusan"]);
+        $nama=input($_POST["id"]);
+        $sekolah=input($_POST["nama"]);
+        $jurusan=input($_POST["alamat"]);
         $no_hp=input($_POST["no_hp"]);
-        $alamat=input($_POST["alamat"]);
-
+	    
         //Query input menginput data kedalam tabel anggota
-        $sql="insert into peserta (nama,sekolah,jurusan,no_hp,alamat) values
-		('$nama','$sekolah','$jurusan','$no_hp','$alamat')";
+        $sql="insert into peserta (id,nama,alamat,no_hp,) values
+		('$id','$nama','$alamat','$no_hp')";
 
         //Mengeksekusi/menjalankan query diatas
         $hasil=mysqli_query($kon,$sql);
@@ -49,27 +48,25 @@
 
     <form action="<?php echo $_SERVER["PHP_SELF"];?>" method="post">
         <div class="form-group">
-            <label>Nama:</label>
-            <input type="text" name="nama" class="form-control" placeholder="Masukan Nama" required />
+            <label>id:</label>
+            <input type="text" id="id" class="form-control" placeholder="Masukan ID" required />
 
         </div>
         <div class="form-group">
-            <label>Sekolah:</label>
-            <input type="text" name="sekolah" class="form-control" placeholder="Masukan Nama Sekolah" required/>
+            <label>nama:</label>
+            <input type="text" name="nama" class="form-control" placeholder="Masukan Nama" required/>
         </div>
-       <div class="form-group">
-            <label>Jurusan :</label>
-            <input type="text" name="jurusan" class="form-control" placeholder="Masukan Jurusan" required/>
-        </div>
-                </p>
+	    
+	 <div class="form-group">
+            <label>Alamat:</label>
+            <textarea name="alamat" class="form-control" rows="5"placeholder="Masukan Alamat" required></textarea>
+        </div>       
+	    
         <div class="form-group">
             <label>No HP:</label>
             <input type="text" name="no_hp" class="form-control" placeholder="Masukan No HP" required/>
         </div>
-        <div class="form-group">
-            <label>Alamat:</label>
-            <textarea name="alamat" class="form-control" rows="5"placeholder="Masukan Alamat" required></textarea>
-        </div>       
+       
 
         <button type="submit" name="submit" class="btn btn-primary">Submit</button>
     </form>
